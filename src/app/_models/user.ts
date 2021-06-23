50cf432d3601876c089cb3c729669bc590b237fe
Isaac Avoseh <avosehisaac0@gmail.com>
2021-06-23 09:57:17 +0100

    angular-commit

---
+++ src/app/_models/user.ts
@@ -0,0 +1,18 @@
+export class User {
+    id: string;
+    username: string;
+    password: string;
+    firstName: string;
+    lastName: string;
+    profession: string;
+    country: string;
+    state: string;
+    lga: string;
+    email: string;
+    phone: number;
+    nin: number;
+    bvn: number;
+    photo: any;
+    idcard: any;
+    token: string;
+}
